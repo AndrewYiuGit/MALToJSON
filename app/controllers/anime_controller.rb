e30629266fb1f7812c5ce38e2538a213f5e74c46
AndrewYiuGit <andrewyiu.git@gmail.com>
2015-06-26 01:10:18 -0700

    Added a config file to grab the credentials from environment variables

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -2,9 +2,19 @@ require 'httparty'
 class AnimeController < ApplicationController
     include HTTParty
 
+    def log_response(response)
+        puts "===================================== Code ====================================="
+        puts response.code
+        puts "===================================== Body ====================================="
+        puts response.body
+    end
+
     def search
         # TODO: Fill these fields with credentials to do basic auth on MAL
-        auth = {:username => "", :password => ""}
+        auth = {
+            :username => APP_CONFIG['mal_username'],
+            :password => APP_CONFIG['mal_password']
+        }
         anime_search_uri = 'http://myanimelist.net/api/anime/search.xml'
         anime_name = params[:title]
 
@@ -13,6 +23,12 @@ class AnimeController < ApplicationController
         anime_search_req = search_uri.to_s
 
         response = HTTParty.get(anime_search_req, :basic_auth => auth)
-        render json: Hash.from_xml(response.body).to_json
+        json_response = {}.to_json
+        if response.code == 200
+            json_response = Hash.from_xml(response.body).to_json
+        else
+            log_response(response)
+        end
+        render json: json_response
     end
 end
